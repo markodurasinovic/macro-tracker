e47d914cadb97d51fae2ad92020cbf85dbc1d3c6
Marko Durasinovic <marko.durasinovic@gmail.com>
2022-11-19 01:23:22 +0000

    make objects used in template optional

--- pages/macro-tracker/macroDisplay.tsx
+++ pages/macro-tracker/macroDisplay.tsx
@@ -2,7 +2,7 @@ import MacroItem from './macroItem'
 
 export default function MacroDisplay({items, handleDeleteItem} : {items: Array<MacroItem>, handleDeleteItem: () => void}) {
     const sum = (key: string) => {
-        return items.reduce((acc, item) => acc + item[key as keyof typeof MacroItem], 0)
+        return items?.reduce((acc, item) => acc + item[key as keyof typeof MacroItem], 0)
     }
 
     return(
@@ -18,7 +18,7 @@ export default function MacroDisplay({items, handleDeleteItem} : {items: Array<M
                     </tr>
                 </thead>
                 <tbody>
-                    {items.map((item, index) => <MacroItem item={item} handleDeleteItem={handleDeleteItem} key={index} />)}
+                    {items?.map((item, index) => <MacroItem item={item} handleDeleteItem={handleDeleteItem} key={index} />)}
                     <tr className='bg-white border-b dark:bg-gray-800 dark:border-gray-700'>
                         <th scope='row' className='py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white'>Total</th>
                         <td className='py-4 px-6'></td>
--- pages/macro-tracker/macroItem.tsx
+++ pages/macro-tracker/macroItem.tsx
@@ -6,10 +6,10 @@ export default function MacroItem({item, handleDeleteItem}: {item: MacroItem, ha
 
     return(
         <tr className='bg-white border-b dark:bg-gray-800 dark:border-gray-700'>
-            <td className='py-4 px-6'>{item.food}</td>
-            <td className='py-4 px-6'>{item.amount}</td>
-            <td className='py-4 px-6'>{item.calories}</td>
-            <td className='py-4 px-6'>{item.protein}</td>
+            <td className='py-4 px-6'>{item?.food}</td>
+            <td className='py-4 px-6'>{item?.amount}</td>
+            <td className='py-4 px-6'>{item?.calories}</td>
+            <td className='py-4 px-6'>{item?.protein}</td>
             <td className='py-4 px-6'><button onClick={deleteItem} className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full'>Delete</button></td>
         </tr>
     )
